220b3c61f8a7e224729b514742ecd5ae27052276
Ashley  Zaki <ahnahn.un@gmail.com>
2018-11-21 19:27:22 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,18 +1,7 @@
 class Dog
-  def initialize(name)
+  def initialize
     @name = name
-  end
-  def name
-    @name
-  end
-  
-  
-  def initialize(breed)
     @breed = "Mutt"
   end
-  def breed
-    @breed
-  end
-  
 end
 
